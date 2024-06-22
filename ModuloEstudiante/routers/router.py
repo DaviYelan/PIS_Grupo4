d9c47ee4c8c9dd7310f8a342e9f5473ccde5515f
Miguel Luna <miguel.a.luna@unl.edu.ec>
2024-06-22 18:11:06 -0500

    Cambios en el modulo

--- ModuloEstudiante/routers/router.py
+++ ModuloEstudiante/routers/router.py
@@ -6,6 +6,14 @@ router = Blueprint('router', __name__)
 #GET es para presentar datos
 #POST guardar datos, modificar datos y el inicio de sesion
 #
+@router.route('/inicio')
+def homeinicio():
+    return render_template("index.html")
+
+@router.route('/calendario')
+def calendario():
+    return render_template("estudiante/calendario.html")
+
 @router.route('/')
 def home():
-    return render_template("template.html")
+    return render_template("index.html")
