eabc289fd1665aaaf4e3f5a29f4241cce7d5111b
DavidL <149276634+DaviYelan@users.noreply.github.com>
2024-06-23 18:47:13 -0500

    Delete Módulo_calificaciones_docente directory
    Cambios en el modulo docente - borrado

--- Módulo_calificaciones_docente/controls/exception/duplicateEntryException.py
+++
@@ -1,5 +0,0 @@
-# exception/DuplicateEntryException.py
-class DuplicateEntryException(Exception):
-    def __init__(self, message="Duplicate entry found"):
-        self.message = message
-        super().__init__(self.message)
