35f802a6e8f0b34fe7ab4b4d3b232c0cb99ab9df
Miguel Luna <miguel.a.luna@unl.edu.ec>
2024-06-25 17:56:21 -0500

    cambios en el main

---
+++ PIS/models/cuenta.py
@@ -0,0 +1,17 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import UserMixin
+
+class Cuenta(UserMixin):
+    def __init__(self, id, correo, clave, role=None):
+        self.id = id
+        self.correo = correo
+        self.clave = clave
+        self.role = role
+
+    @classmethod
+    def check_password(cls, hashed_password, clave):
+        return check_password_hash(hashed_password, clave)
+
+    #print(generate_password_hash("grupocuatro"))
+    #print(generate_password_hash("lore"))
+    #print(generate_password_hash("456"))
