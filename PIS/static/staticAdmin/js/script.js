7763d62b52df9f5a3ee64f7b59806a92bc00ce43
Miguel Luna <miguel.a.luna@unl.edu.ec>
2024-07-20 11:54:06 -0500

    cambios en el main

--- PIS/static/staticAdmin/js/script.js
+++ PIS/static/staticAdmin/js/script.js
@@ -5,22 +5,32 @@ const body = document.querySelector('body'),
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
 
+// Verificar el estado del modo oscuro al cargar la página
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        body.classList.add('dark');
+        modeText.innerText = "Light mode";
+    } else {
+        modeText.innerText = "Dark mode";
+    }
+});
 
-toggle.addEventListener("click" , () =>{
+toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
-})
+});
 
-searchBtn.addEventListener("click" , () =>{
+searchBtn.addEventListener("click", () => {
     sidebar.classList.remove("close");
-})
+});
 
-modeSwitch.addEventListener("click" , () =>{
+modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
     
-    if(body.classList.contains("dark")){
+    if (body.classList.contains("dark")) {
         modeText.innerText = "Light mode";
-    }else{
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
         modeText.innerText = "Dark mode";
-        
+        localStorage.setItem('darkMode', 'disabled');
     }
 });
