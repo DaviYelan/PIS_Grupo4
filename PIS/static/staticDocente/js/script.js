497285810fab2b5c7fa26b16c771bf120b02547e
Miguel Luna <miguel.a.luna@unl.edu.ec>
2024-07-20 12:08:47 -0500

    Modificacion en lógica Inicio de sesión

--- PIS/static/staticDocente/js/script.js
+++ PIS/static/staticDocente/js/script.js
@@ -5,14 +5,18 @@ const body = document.querySelector('body'),
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
 
+// Elimina la clase 'close' del sidebar cuando la página se carga
+document.addEventListener("DOMContentLoaded", () => {
+    sidebar.classList.remove("close");
+});
 
 toggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
-})
+});
 
 searchBtn.addEventListener("click" , () =>{
     sidebar.classList.remove("close");
-})
+});
 
 modeSwitch.addEventListener("click" , () =>{
     body.classList.toggle("dark");
@@ -21,6 +25,5 @@ modeSwitch.addEventListener("click" , () =>{
         modeText.innerText = "Light mode";
     }else{
         modeText.innerText = "Dark mode";
-        
     }
 });
--- PIS/static/staticMensajes/js/loader.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", (event) => {
-  const loader = document.getElementById("loader-out");
-  setTimeout(function () {
-    loader.style.opacity = "0";
-    setTimeout(function () {
-      loader.style.display = "none";
-    }, 400);
-  }, 400);
-});
