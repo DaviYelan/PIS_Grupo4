eed54c5bfe4e1651327a49d6fe45ab6bcefe42c3
Miguel Luna <miguel.a.luna@unl.edu.ec>
2024-07-22 03:24:21 -0500

    eliminacion de archivos innecesarios

--- PIS/models/componenteRubrica.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class ComponenteRubrica(Enum):
-    ACD = "ACD"
-    APE = "APE"
-    AA = "AA"
-    EVALUACION = "EVALUACION"
--- PIS/models/enumComponenteRubrica.py
+++
@@ -1,9 +0,0 @@
-import enum
-class EnumComponenteRubrica(enum.Enum):
-    ACD = "ACD"
-    APE = "APE"
-    AA = "AA"
-    EVALUACION = "EVALUACION"
-
-    def getValue(self):
-        return self.value
